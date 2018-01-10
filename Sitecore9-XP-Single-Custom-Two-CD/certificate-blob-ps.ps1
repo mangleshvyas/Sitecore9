b6c26c45bf02cf3a9205b5ea2506e3c04d0ec267
Manglesh Vyas <mvyas@horizontalintegration.com>
2018-01-10 16:55:33 +0530

    Sitecore9-XP-Custome

---
+++ Sitecore9-XP-Single-Custom-Two-CD/certificate-blob-ps.ps1
@@ -0,0 +1,15 @@
+﻿#generate certificate
+$thumbprint = (New-SelfSignedCertificate `
+    -Subject "CN=$env:COMPUTERNAME @ Sitecore, Inc." `
+    -Type SSLServerAuthentication `
+    -FriendlyName "$env:USERNAME Certificate").Thumbprint
+#export certificate with password
+$certificateFilePath = "C:\Temp\$thumbprint.pfx"
+Export-PfxCertificate `
+    -cert cert:\LocalMachine\MY\$thumbprint `
+    -FilePath "$certificateFilePath" `
+    -Password (Read-Host -Prompt "Enter password that would protect the certificate" -AsSecureString)
+#convert it to base64 string (blob)
+$fileContentBytes = get-content $certificateFilePath -Encoding Byte
+[System.Convert]::ToBase64String($fileContentBytes) | Out-File "C:\Temp\$thumbprint.txt"
+Write-Host "Your secure certificate blob is located at C:\Temp\$thumbprint.txt" 
